72cf505bce088b9eea6b79259b2638df0f97f017
linkerbs <00002918@uca.edu.sv>
2019-11-23 13:26:36 -0600

    cambios al .gitignore

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -6,17 +6,18 @@ class api extends \Restserver\Libraries\REST_Controller {
  parent::__construct($config);
  } 
  
- function index_get() {
- $id = $this->get('Id');
- if ($id == '') {
- $product = $this->db->get('plan')->result();
- } else {
- $this->db->where('Id', $id);
- $product = $this->db->get('plan')->result();
+ function index_get($id = 0) {
+    if(!empty($id)){
+    $plan = $this->db->get_where("plan", ['Id' => $id])->row_array();
+    }
+    else
+    {
+    $plan = $this->db->get("plan")->result();
+    }
+    $this->response($plan, 200);
  }
- $this->response($product, 200);
- }
- 
+
+
  function index_post() {
  $data = array(
  'proveedor' => $this->input->post('proveedor'),
@@ -26,26 +27,26 @@ class api extends \Restserver\Libraries\REST_Controller {
  );
  $insert = $this->db->insert('plan', $data);
  if ($insert) {
- $this->response($data, 200);
+ $this->response($data, 201);
  } else {
- $this->response(array('status' => 'fail', 502));
+ $this->response(array('status' => 'fail', 400));
  }
  }
  
  function index_put() {
  $id = $this->put('Id');
  $data = array(
- 'proveedor' => $this->input->post('proveedor'),
- 'velocidad' => $this->input->post('velocidad'),
- 'calidad' => $this->input->post('calidad'),
- 'precio' => $this->input->post('precio'),
+ 'proveedor' => $this->put('proveedor'),
+ 'velocidad' => $this->put('velocidad'),
+ 'calidad' => $this->put('calidad'),
+ 'precio' => $this->put('precio'),
  );
  $this->db->where('Id', $id);
  $update = $this->db->update('plan', $data);
  if ($update) {
  $this->response($data, 200);
  } else {
- $this->response(array('status' => 'fail', 502));
+ $this->response(array('status' => 'fail', 400));
  }
  }
  
@@ -54,9 +55,9 @@ class api extends \Restserver\Libraries\REST_Controller {
  $this->db->where('Id', $id);
  $delete = $this->db->delete('plan');
  if ($delete) {
- $this->response(array('status' => 'success'), 201);
+ $this->response(array('status' => 'success'), 200);
  } else {
- $this->response(array('status' => 'fail', 502));
+ $this->response(array('status' => 'fail', 404));
  }
  }
 }
